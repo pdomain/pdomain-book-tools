562355009123a3f05fcd718cfb80bb5709add85c
CT <ConcaveTrillion@gmail.com>
2025-04-19 07:39:15 -0400

    ocr updates

--- pd_book_tools/ocr/page.py
+++ pd_book_tools/ocr/page.py
@@ -60,7 +60,7 @@ class Page:
     _cv2_numpy_page_image_word_with_bboxes_and_gt_text: Optional[ndarray] = None
     _cv2_numpy_page_image_matched_word_with_colors: Optional[ndarray] = None
 
-    unmatched_ground_truth_lines: list[(int, str)] = None
+    unmatched_ground_truth_lines: "list[(int, str)]" = None
     "List of Ground Truth Lines and the line they were found on before an OCR match"
 
     def __init__(
