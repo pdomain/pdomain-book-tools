086579750b9749a2fc51461954f2a40188ded59c
ConcaveTrillion <concavetrillion@gmail.com>
2026-06-01 00:17:29 +0000

    chore(page): export GtOrphans and BlobStoreProtocol from ocr package

--- pdomain_book_tools/ocr/__init__.py
+++ pdomain_book_tools/ocr/__init__.py
@@ -1,5 +1,7 @@
 """OCR helpers for pdomain-book-tools."""
 
+from pdomain_book_tools.ocr.blob_protocol import BlobStoreProtocol
+from pdomain_book_tools.ocr.gt_orphans import GtOrphans
 from pdomain_book_tools.ocr.text_normalize import (
     apply_text_normalizations,
     normalize_curly_quotes,
@@ -7,6 +9,8 @@ from pdomain_book_tools.ocr.text_normalize import (
 )
 
 __all__ = [
+    "BlobStoreProtocol",
+    "GtOrphans",
     "apply_text_normalizations",
     "normalize_curly_quotes",
     "normalize_em_dash",
